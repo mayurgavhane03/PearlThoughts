4602a21a00e85dc28de55e95ab52c77b535c8f5c
GAVHANE MAYUR RAJENDRA <2020bec075@sggs.ac.in>
2024-09-15 23:32:59 +0530

    preview buttom removed

--- src/app/Components/DatePicker.tsx
+++ src/app/Components/DatePicker.tsx
@@ -1,45 +1,56 @@
- "use client"
- import React from 'react';
- import RecurrenceOptions from './RecurrenceOptions';
- import DateRangePicker from './DateRangePicker';
- import RecurrencePreview from './RecurrencePreview';
- import { generateRecurringDates } from '../utils/generateRecurringDates';
-import { useDatePickerStore } from './store/useDatePickerStore';
- 
- const DatePicker: React.FC = () => {
-   const { recurrenceType, interval, startDate, endDate, daysOfWeek, nthDay, setGeneratedDates } =
-     useDatePickerStore();
- 
-   const handleGeneratePreview = () => {
-     const dates = generateRecurringDates({
-       recurrenceType,
-       interval,
-       startDate,
-       endDate,
-       daysOfWeek,
-       nthDay,
-     });
-     setGeneratedDates(dates); // Update the store with the generated dates
-   };
- 
-   return (
-     <div className="bg-white p-4 shadow-md rounded-lg">
-       <h2 className="text-xl text-black font-semibold mb-4">Recurring Date Picker</h2>
-       <RecurrenceOptions />
-       <DateRangePicker />
-       
- 
-       <button
-         onClick={handleGeneratePreview}
-         className="bg-blue-500 text-white rounded-md p-2 mt-4"
-       >
-         Generate Preview
-       </button>
-       
-       <RecurrencePreview />
-     </div>
-   );
- };
- 
- export default DatePicker;
- 
+"use client";
+import React, { useEffect } from "react";
+import RecurrenceOptions from "./RecurrenceOptions";
+import DateRangePicker from "./DateRangePicker";
+import RecurrencePreview from "./RecurrencePreview";
+import { generateRecurringDates } from "../utils/generateRecurringDates";
+import { useDatePickerStore } from "./store/useDatePickerStore";
+
+const DatePicker: React.FC = () => {
+  const {
+    recurrenceType,
+    interval,
+    startDate,
+    endDate,
+    daysOfWeek,
+    nthDay,
+    setGeneratedDates,
+  } = useDatePickerStore();
+
+  useEffect(() => {
+    if (endDate) {
+      // Generate preview when endDate is available
+      const dates = generateRecurringDates({
+        recurrenceType,
+        interval,
+        startDate,
+        endDate,
+        daysOfWeek,
+        nthDay,
+      });
+      setGeneratedDates(dates); // Update the store with the generated dates
+    }
+  }, [
+    endDate,
+    recurrenceType,
+    interval,
+    startDate,
+    daysOfWeek,
+    nthDay,
+    setGeneratedDates,
+  ]);
+
+  return (
+    <div className="bg-white p-4 shadow-md rounded-lg">
+      <h2 className="text-xl text-black font-semibold mb-4">
+        Recurring Date Picker
+      </h2>
+      <RecurrenceOptions />
+      <DateRangePicker />
+
+      <RecurrencePreview />
+    </div>
+  );
+};
+
+export default DatePicker;
